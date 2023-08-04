a15168104c6f19a1ebf14142f79468e12d21e76c
Chirag A K <chiragajekar@gmail.com>
2023-08-04 14:28:43 +0530

    .

---
+++ coe/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <div class="copyright">
+    Copyright &copy; 2023 CoE Digital Forensics Intelligence & Cyber Security-SCEM
+  </div>
+</footer>
+
---
+++ coe/includes/header.php
@@ -0,0 +1,20 @@
+<nav class="nav">
+    <div class="container">
+        <div class="logo">
+            <a href="#">
+                <img src="images/Logo.png" id="Logo">
+                <h1 class="logo-title">CoE Digital Forensics <br>Intelligence & Cyber Security-SCEM</h1>
+            </a>
+        </div>
+        <div id="mainListDiv" class="main_list">
+            <ul class="navlinks">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Skills Hub</a></li>
+                <li><a href="#">Resources Hub</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#"><i class="fa fa-fw fa-user"></i> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
